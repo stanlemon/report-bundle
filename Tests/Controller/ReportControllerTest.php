21183f2e4e2c862170a73e77566b85f333a7e670
Stan Lemon <stosh1985@gmail.com>
2014-01-31 21:36:35 -0500

    Stubbing test suite setup

---
+++ Tests/Controller/ReportControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace Lemon\ReportBundle\Test\Controller;
+
+class ReportControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testListAction()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testViewAction()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testJsonAction()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCsvAction()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testXmlAction()
+    {
+        $this->markTestIncomplete();
+    }
+}
