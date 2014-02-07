2b2be9d4f96c3d3dbe9e0e3126ed7aef7c0d5024
Stan Lemon <stosh1985@gmail.com>
2014-02-06 20:42:05 -0500

    Removed more unused use statements.

--- Report/Loader/ChainRepository.php
+++ Report/Loader/ChainRepository.php
@@ -2,10 +2,7 @@
 
 namespace Lemon\ReportBundle\Report\Loader;
 
-use Doctrine\ORM\EntityRepository;
 use Psr\Log\LoggerInterface;
-
-use Lemon\ReportBundle\Report\Loader\LoaderInterface;
 
 class ChainRepository implements RepositoryInterface
 {
